0f9b25014b5bae157038f790dd19e81e8c9bdc71
gramcha <gramcha@pando.in>
2025-10-23 12:14:28 +0530

    adding adding sequalizecli part of image.

--- build.sh
+++ build.sh
@@ -23,5 +23,15 @@
 #docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node18-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
 
 # ubuntu 22.04, node v22.8.0, nginx 1.18.0, consul 1.9.4, wesyprint
-version=7.0.2
+#version=7.0.2
+#docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node22-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
+#docker buildx build --push --tag 669650451927 .dkr.ecr.ap-south-1.amazonaws
+# .com/ubuntu-node-nginx:$version -f Dockerfile-node22-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
+
+# adding sequalizecli part of image.
+version=7.0.3
 docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node22-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
+#docker buildx build --push --tag 669650451927 .dkr.ecr.ap-south-1.amazonaws
+# .com/ubuntu-node-nginx:$version -f Dockerfile-node22-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
+#docker buildx build --push --tag codebuildext/ubuntu-node-nginx:$version -f Dockerfile-node22-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
+docker buildx build --push --tag gramcha/ubuntu-node-nginx:7.0.3 -f Dockerfile-node22-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
